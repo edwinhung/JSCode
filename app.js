f25736842bcb42300ee7f8c5b763da0b5ff70751
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-05-21 14:31:42 -0700

    function call without required argument

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 console.log(3+4);
 console.error("OH NO!!!");
+
+function divide (a,b) {
+    console.log(b);
+    return a / b;
+}
+
+console.log(divide(5)); // when required argument not passed, it will be undefined.
