811abcf58ab518a3d5816641cd901c8151d81e80
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-05-24 09:58:14 -0700

    add avg function

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-console.log(3+4);
-console.error("OH NO!!!");
-
-function divide (a,b) {
-    console.log(b);
-    return a / b;
+function find_missing(arr1, arr2) {
+    arr1.sort();
+    arr2.sort();
+    for (let i = 0; i < arr2.length; i++) {
+        if (arr1[i] !== arr2[i]) return(arr1[i]);
+    }
+    return arr1[arr1.length - 1];
 }
 
-console.log(divide(5)); // when required argument not passed, it will be undefined.
+console.log(find_missing([1, 2, 2, 3], [1, 2, 3]));
+console.log(find_missing([6, 1, 3, 6, 8, 2], [3, 6, 6, 1, 2]));
+
+function avg(arr) {
+    sum = 0;
+    for (let i of arr) {
+        sum += i;
+    }
+    return sum / arr.length;
+}
+
+console.log(avg([1,2,3,4,5,10]));
