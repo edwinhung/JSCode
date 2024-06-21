a7944922cdeb0d158220ccb808933b40a04ce007
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-06-21 11:47:21 -0700

    refactor parallel to use Promise.all

--- app.js
+++ app.js
@@ -248,14 +248,14 @@ async function get3PokemonPara() {
   const prom1 = axios.get("https://pokeapi.co/api/v2/pokemon/2");
   const prom2 = axios.get("https://pokeapi.co/api/v2/pokemon/3");
   const prom3 = axios.get("https://pokeapi.co/api/v2/pokemon/4");
-  console.log(prom1);
-  const poke1 = await prom1;
-  const poke2 = await prom2;
-  const poke3 = await prom3;
-  console.log(prom1);
-  console.log(poke1.data.name);
-  console.log(poke2.data.name);
-  console.log(poke3.data.name);
+  const results = await Promise.all([prom1, prom2, prom3]);
+  printPokemons(results);
+}
+
+function printPokemons(results) {
+  for (let result of results) {
+    console.log(result.data.name);
+  }
 }
 
 (async () => {
