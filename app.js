98b64766cf305e07a475ff46b69529c36b482b73
Edwin Hung <32249535+edwinhung@users.noreply.github.com>
2024-06-20 18:26:57 -0700

    refactor to parallel requests

--- app.js
+++ app.js
@@ -234,10 +234,25 @@
 //     );
 //   });
 // Sequential
+// async function get3Pokemon() {
+//   const poke1 = await axios.get("https://pokeapi.co/api/v2/pokemon/2");
+//   const poke2 = await axios.get("https://pokeapi.co/api/v2/pokemon/3");
+//   const poke3 = await axios.get("https://pokeapi.co/api/v2/pokemon/4");
+//   console.log(poke1.data.name);
+//   console.log(poke2.data.name);
+//   console.log(poke3.data.name);
+// }
+
+// Parallell
 async function get3Pokemon() {
-  const poke1 = await axios.get("https://pokeapi.co/api/v2/pokemon/2");
-  const poke2 = await axios.get("https://pokeapi.co/api/v2/pokemon/3");
-  const poke3 = await axios.get("https://pokeapi.co/api/v2/pokemon/4");
+  const prom1 = axios.get("https://pokeapi.co/api/v2/pokemon/2");
+  const prom2 = axios.get("https://pokeapi.co/api/v2/pokemon/3");
+  const prom3 = axios.get("https://pokeapi.co/api/v2/pokemon/4");
+  console.log(prom1);
+  const poke1 = await prom1;
+  const poke2 = await prom2;
+  const poke3 = await prom3;
+  console.log(prom1);
   console.log(poke1.data.name);
   console.log(poke2.data.name);
   console.log(poke3.data.name);
